c353f76bb27bd2544c843a666925427a3a7534da
Akin <ogundareakinniyi8@gmail.com>
2024-05-16 22:20:29 +0100

    added webdebugging file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,10 @@
-# this puppet conf increases how available working file process for apache
-
-# change available limit to 4096 from 15
-exec {'fix-limit':
-    command => '/bin/sed -i "/s/15/4096/g" /etc/default/nginx',
-    path    => '/usr/local/bin/:/bin/',
+# Fix the nginx requests limit
+exec { 'upgrade':
+  path    => '/bin/',
+  command => 'sed -i "s/15/4096" /etc/default/nginx',
 }
 
-#restart the server
-exec {'nginx-restart':
-    command => '/etc/init.d/nginx restart',
-    path    => '/etc/init.d',
+exec { 'restart':
+  path    => '/usr/bin/',
+  command => 'service nginx restart',
 }
